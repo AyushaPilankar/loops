ff4c74ff190f6d91a70e9fbbba2228169806ca97
Ayusha <ayushapilankar@gmail.com>
2026-08-31 20:05:38 +0530

    plusoperatorcode

--- src/string/plusoperater.java
+++ src/string/plusoperater.java
@@ -1,5 +1,11 @@
 package string;
 
 public class plusoperater {
+    public static void main(String[] args) {
+        String s="bhakti";
+        s=s+"hi shakti hai";
+        System.out.println(s);
+
+    }
 
 }
