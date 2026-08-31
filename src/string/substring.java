9d3a4f9cd2e6bce3cf36894eb17d86439f61d27b
Ayusha <ayushapilankar@gmail.com>
2026-08-31 20:28:46 +0530

    substing indexing

--- src/string/substring.java
+++ src/string/substring.java
@@ -1,5 +1,10 @@
 package string;
 
 public class substring {
-    p
+    public static void main(String[] args) {
+        String s="ayusha";
+        System.out.println(s.substring(1,5));
+        System.out.println(s.substring(4));
+
+    }
 }
