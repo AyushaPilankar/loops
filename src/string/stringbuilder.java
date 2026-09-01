a171b652d38ffc98c37446e476e83185d65f4488
Ayusha <ayushapilankar@gmail.com>
2026-09-01 20:29:56 +0530

    Reversing String builder

--- src/string/stringbuilder.java
+++ src/string/stringbuilder.java
@@ -1,4 +1,11 @@
-package string;
-
-public class stringbuilder {
-}
+//package string;
+//import java.util.ArrayList;
+//
+//public class stringbuilder {
+//    public static void main(String[] args) {
+//        String Builder s=new String Builder("Raghav");
+//        System.out.println(s.length()+" "+s.capacity());
+//        System.out.println(s);
+//    }
+//
+//}
