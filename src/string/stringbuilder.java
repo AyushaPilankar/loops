774728c886afb82e1b82d3faae89ed30ca18ea2d
Ayusha <ayushapilankar@gmail.com>
2026-09-01 21:07:48 +0530

    stringbuildercode

--- src/string/stringbuilder.java
+++ src/string/stringbuilder.java
@@ -1,11 +1,11 @@
-//package string;
-//import java.util.ArrayList;
-//
-//public class stringbuilder {
-//    public static void main(String[] args) {
-//        String Builder s=new String Builder("Raghav");
-//        System.out.println(s.length()+" "+s.capacity());
-//        System.out.println(s);
-//    }
-//
-//}
+package string;
+import java.util.ArrayList;
+
+public class stringbuilder {
+    public static void main(String[] args) {
+        StringBuilder s=new StringBuilder("Raghav");
+        System.out.println(s.length()+" "+s.capacity());
+        System.out.println(s);
+    }
+
+}
